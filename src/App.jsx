8d1c05459e0137bf6ba2c316d7271ad6c9213d26
kebal khadka <kebalkhadka7@gmail.com>
2024-08-06 17:39:30 +0545

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
     <>
      <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center'>
      <CurrencyConverter/>
-     {/* 91e7a604ce3467e1e387bd5a */}
      </div>
     </>
   )
