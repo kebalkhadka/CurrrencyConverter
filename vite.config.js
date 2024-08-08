7e530d377488b3d56ada4f4c79217de34c8f3064
kebal khadka <kebalkhadka7@gmail.com>
2024-08-08 18:06:59 +0545

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CurrrencyConverter/', // Ensure this matches your GitHub repository name exactly
   plugins: [react()],
 })
